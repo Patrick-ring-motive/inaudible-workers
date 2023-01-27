4eecda74cc6b0c87bdeff1d8c8af126bc4049904
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-27 12:33:10 -0600

    Update inaudible-worker.js

--- InaudibleWorker/inaudible-worker.js
+++ InaudibleWorker/inaudible-worker.js
@@ -30,15 +30,15 @@ console.warn = window.customWarn;
   
   
 window.gestureState = async function(){
-while(window.gestureAttemptCount<=window.gestureFailCount){
-await sleep(100);
-new InaudibleContext();
-console.log('ga',window.gestureAttemptCount);
-console.log('ga',window.gestureFailCount);
-}
+  while(window.gestureAttemptCount<=window.gestureFailCount){
+    await sleep(100);
+    new InaudibleContext();
+    console.log('ga',window.gestureAttemptCount);
+    console.log('ga',window.gestureFailCount);
+  }
 }
 
-
+window.gestureState();
 
 window.InaudibleWorker = class InaudibleWorker {
  constructor(workerURL) {
