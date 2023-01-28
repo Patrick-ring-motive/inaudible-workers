19b050d40d91668d6cc339b5ba257c980f5a5f9c
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-01-27 21:22:58 -0600

    Update inaudible-processor.js

--- InaudibleWorker/inaudible-processor.js
+++ InaudibleWorker/inaudible-processor.js
@@ -2,8 +2,17 @@ class MyAudioProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
     this.port.onmessage=function(){console.log('port message received');};
+    this.postMessage = this.port.postMessage;
     console.log('processor created');
   }
+  
+  set onmessage(msg){
+  
+  this.port.onmessage = msg;
+  return this.port.onmessage;
+  
+  }
+  
   
   process(inputList, outputList, parameters) {
     return true;
