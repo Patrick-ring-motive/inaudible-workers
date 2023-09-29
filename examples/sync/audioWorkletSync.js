414eb1caef02c5c762b32f288735415216c1e437
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-09-28 21:28:44 -0500

    Update audioWorkletSync.js

--- examples/sync/audioWorkletSync.js
+++ examples/sync/audioWorkletSync.js
@@ -110,7 +110,18 @@ void function WorkletProcessor(){
       super();
   this.self=this;
   globalThis.self=this;
- // WORKLETSCRIPT
+      
+self.onmessage = function(e) {
+  console.log('Worker: Message received from main script');
+  const result = e.data[0] * e.data[1];
+  if (isNaN(result)) {
+    self.postMessage('Please write two numbers');
+  } else {
+    const workerResult = 'Result: ' + result;
+    console.log('Worker: Posting message back to main script');
+    self.postMessage(workerResult);
+  }
+}
       console.log('processor created');
     }
     
