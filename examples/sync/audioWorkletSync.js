a68091ef792349511aaec6303f437c96ba4e7364
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-09-28 22:22:26 -0500

    Update audioWorkletSync.js

--- examples/sync/audioWorkletSync.js
+++ examples/sync/audioWorkletSync.js
@@ -108,7 +108,7 @@ void async function WorkletBuilder(){
 
   let source = myWorker.audioContext.createBufferSource();
 
-  let data = new ArrayBuffer(128);
+  let data = await (await fetch('https://patrick-ring-motive.github.io/inaudible-workers/examples/sync/viper.ogg')).arrayBuffer();//new ArrayBuffer(128);
   myWorker.audioContext.decodeAudioData(data,(buffer)=>{
       source.buffer = buffer;
       source.connect(myWorker.audioContext.destination);
